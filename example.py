e7fd3870e70833f84c7d46ae9a0c6eb43fb5ff47
Andreas Jansson <andreas@jansson.me.uk>
2013-01-23 22:37:28 +0000

    tidy up example

--- example.py
+++ example.py
@@ -8,10 +8,13 @@ import chromeheap
 if __name__ == '__main__':
     chrome_options = Options()
     chrome_options.add_argument('--remote-debugging-port=9222')
-    chrome_options.add_argument('--user-data-dir=/tmp/jsleakcheck') # assumes logged in
+    chrome_options.add_argument('--user-data-dir=/tmp/chromeheap')
     chrome_options.add_argument('--js-flags=--stack_trace_limit=-1')
     driver = webdriver.Chrome(chrome_options=chrome_options)
+
     driver.get('http://www.thisismyjam.com')
 
     stats = chromeheap.stats()
     print 'heap size: %s, node count: %d\n' % stats
+
+    driver.close()
